db9ca904ae6cc6dac9e020f2351ea0a789e2b975
gsinuiri-coder <gsinuiri@gmail.com>
2021-06-06 06:39:15 -0500

    first commit and code configuration

---
+++ src/test/java/com/gyasociety/CarryDoggyGoService/CarryDoggyGoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyasociety.CarryDoggyGoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarryDoggyGoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
